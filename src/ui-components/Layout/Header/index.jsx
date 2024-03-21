88891f2fd92d05c7fefa346221b84db4417bc9aa
galarcon-ccg <galarcon@ccg.unam.mx>
2024-03-21 15:12:01 -0600

    algo aqui que no termine

--- src/ui-components/Layout/Header/index.jsx
+++ src/ui-components/Layout/Header/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./header.css"
+import { isMobile } from 'react-device-detect';
 
 export default function Header() {
+  if (isMobile) {
+    return <div style={{width: "100%", height: "5px", backgroundColor: "#1f3d4e"}} ></div>
+  }
   return (
     <div className='rdb_Layout_Header'>
         <div className="Logo">
-
         </div>
         <div className="Links"></div>
       </div>
