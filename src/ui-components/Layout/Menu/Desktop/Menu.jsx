013818305bd670555e77e5228138c79b5f36e13d
galarcon-ccg <galarcon@ccg.unam.mx>
2024-03-21 12:02:39 -0600

    implement menu Options

---
+++ src/ui-components/Layout/Menu/Desktop/Menu.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import List from "@mui/material/List";
+import Typography from '@mui/material/Typography';
+
+export default function Menu({menu, setMenu=()=>{}}) {
+    const navigate = useNavigate();
+    return (
+      <div
+        style={{
+          display: "flex",
+          overflow: "auto",
+          backgroundColor: "#2c4f63",
+          padding: "20px 10% 15px 10%",
+        }}
+        onMouseLeave={() => {
+          setMenu(undefined);
+        }}
+      >
+        {menu.options.map((menu) => {          
+          return (
+            <List
+              dense
+              key={menu.id}
+              sx={{ width: "100%", maxWidth: 360, bgcolor: "transparent", color:"white" }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+              subheader={
+                <Typography variant='relevant' sx={{color: "white"}} >
+                    {menu.label}
+                </Typography>
+              }
+            >
+              {menu.options.map((menu,index) => {
+                if (menu.disabled) {
+                  return null;
+                }
+                return (
+                  <ListItemButton
+                  key={"listItemButton_"+menu.label+"_"+index}
+                    onClick={() => {
+                      navigate(menu.link);
+                      setMenu()
+                    }}
+                  >
+                    <ListItemText
+                    sx={{
+                        ".MuiListItemText-primary": {
+                            color: "white"
+                        }
+                    }}
+                    primary={menu.label} />
+                  </ListItemButton>
+                );
+              })}
+            </List>
+          );
+        })}
+      </div>
+    );
+  }
--- src/ui-components/Layout/Menu/Desktop/index.jsx
+++ src/ui-components/Layout/Menu/Desktop/index.jsx
@@ -1,35 +1,44 @@
-import React, { useState } from 'react'
-import "./style.css"
-import IconButton from '@mui/material/IconButton';
-import HomeIcon from '@mui/icons-material/Home';
-import Options from './Options';
-import menus from '../conf'
+import React, { useState } from "react";
+import "./style.css";
+import IconButton from "@mui/material/IconButton";
+import HomeIcon from "@mui/icons-material/Home";
+import Options from "./Options";
+import Menu from "./Menu";
+import menus from "../conf";
 
 export default function Desktop() {
-    const [menu, setMenu] = useState()
-    //console.log(menu?.id);
-    const handleSelectMenu = (menu)=>{
-        setMenu(menu)
-    }
-    const handleCloseMenu = ()=>{
-        setMenu(undefined)
-    }
-    return (
-        <div className="rdb_Layout_Desktop">
-            <div className="options">
-                <div style={{ width: "5vw", }}>
-
-                </div>
-                <div className="home-button" >
-                    <IconButton sx={{
-                        height: "5vw",
-                        width: "5vw",
-                        maxHeight: "60px",
-                        maxWidth: "60px"
-                    }} ><HomeIcon className='home-button-icon' sx={{ color: "white" }} /></IconButton>
-                </div>
-                <Options menus={menus} menuId={menu?.id} handleSelectMenu={handleSelectMenu} handleCloseMenu={handleCloseMenu} />
-            </div>
+  const [menu, setMenu] = useState();
+  //console.log(menu?.id);
+  const handleSelectMenu = (menu) => {
+    setMenu(menu);
+  };
+  const handleCloseMenu = () => {
+    setMenu(undefined);
+  };
+  return (
+    <div className="rdb_Layout_Desktop">
+      <div className="options">
+        <div style={{ width: "3vw" }}></div>
+        <div className="home-button">
+          <IconButton
+            sx={{
+              height: "5vw",
+              width: "5vw",
+              maxHeight: "60px",
+              maxWidth: "60px",
+            }}
+          >
+            <HomeIcon className="home-button-icon" sx={{ color: "white" }} />
+          </IconButton>
         </div>
-    )
+        <Options
+          menus={menus}
+          menuId={menu?.id}
+          handleSelectMenu={handleSelectMenu}
+          handleCloseMenu={handleCloseMenu}
+        />
+      </div>
+      {menu && <Menu menu={menu} />}
+    </div>
+  );
 }
