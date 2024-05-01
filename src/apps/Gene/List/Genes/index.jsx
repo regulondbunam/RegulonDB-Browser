e434e773ec689d1071594b4db54485d722651779
galarcon-ccg <55556303+galarcon-ccg@users.noreply.github.com>
2024-05-01 10:00:42 -0600

    implement filter table

--- src/apps/Gene/List/Genes/index.jsx
+++ src/apps/Gene/List/Genes/index.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from "@mui/material/Typography";
 import LinearProgress from '@mui/material/LinearProgress';
+import { Button, ButtonGroup, Tooltip } from '@mui/material'
 import { List } from 'ui-components/Web/List';
+import FilterTable from 'ui-components/Web/FilterTable';
+const views = {
+  list: {
+    label: "List",
+    tooltip: "view genes in list",
+    component: List
+  },
+  table: {
+    label: "Table",
+    tooltip: "view genes in table",
+    component: FilterTable
+  }
+}
 
 export default function GeneList({ data, loading = false }) {
+  const [view, setView] = useState(views.list)
+  const { label, component } = view
   return (
-    <List
-      elevation={0}
-      square
-      data={data}
-      title={loading ? (<>
-        <Typography gutterBottom variant="irrelevant" component="div">
-          {"loading..."}
-        </Typography>
-        <LinearProgress />
-      </>) : (
-        <Typography gutterBottom variant="relevant" component="div">
-          {"" + data.length + " genes"}
-        </Typography>
-      )}
-      pagination />
+    <div style={{ padding: "16px 3% 16px 15px" }}>
+      <div style={{ display: 'flex', alignItems: "center", justifyContent: "space-between" }} >
+        <div>
+          {loading ? (<>
+            <Typography variant="irrelevant" component="div">
+              {"loading..."}
+            </Typography>
+          </>) : (
+            <Typography variant="relevant" component="div">
+              {"" + data.length + " genes"}
+            </Typography>
+          )}
+        </div>
+        <div>
+          <ButtonGroup size='small' color="secondary" variant="contained" aria-label="Basic button group">
+            {Object.keys(views).map((key) => (
+              <Tooltip title={views[key].tooltip}>
+                <Button onClick={() => { setView(views[key]) }} >{views[key].label}</Button>
+              </Tooltip>
+            ))}
+          </ButtonGroup>
+        </div>
+      </div>
+      <List
+        data={data}
+        pagination />
+    </div>
   )
 }
+
+/* 
+      title=
+*/
--- src/ui-components/Web/List/index.jsx
+++ src/ui-components/Web/List/index.jsx
@@ -4,44 +4,45 @@ import MUIList from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import { Typography, Button, ButtonGroup, Divider } from '@mui/material'
+import Tooltip from "@mui/material/Tooltip"
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export function List(props) {
     const {
         title = "",
         data = [],
         pagination = false,
-        limit: _limit = 10,
+        limit: _limit = 5,
     } = props
     const [page, setPage] = useState(1)
     // eslint-disable-next-line no-unused-vars
-    const [limit, setlimit] = useState(_limit)
+    const [limit, setLimit] = useState(_limit)
     let nResults = 0
     if (DataVerifier.isValidArray(data)) {
         nResults = data?.length ? data.length : 0
     }
-    const handelFirstPage = () => {
+    const handleFirstPage = () => {
         setPage(1)
     }
-    const handelPrevPage = () => {
+    const handlePrevPage = () => {
         if (page > 1) {
             setPage(p => p - 1)
         }
     }
-    const handelNextPage = () => {
+    const handleNextPage = () => {
         if (nResults / limit > page) {
             setPage(p => p + 1)
         }
     }
-    const handelLastPage = () => {
+    const handleLastPage = () => {
         setPage(Math.ceil(nResults / limit))
     }
     return (
-        <Card {...props}>
-            <CardContent>
+        <div>
+            <div>
                 {title}
                 <Divider />
                 <MUIList
@@ -86,28 +87,48 @@ export function List(props) {
                     )}
 
                 </MUIList>
-                <Divider/>
-            </CardContent>
+                <Divider />
+            </div>
             {(pagination && DataVerifier.isValidArray(data)) && (
-                <CardActions>
-                    <div style={{ display: "flex", alignItems: "center", width: "100%", justifyContent: "flex-end" }}>
-                        <div style={{ marginRight: "10px" }} >
-                            <Typography variant='irrelevant' > {`${limit} results shown out of ${nResults}`} </Typography>
-                        </div>
-                        <ButtonGroup variant="contained" color='secondary' size='small' aria-label="Basic button group">
-                            <Button onClick={handelFirstPage} disabled={page === 1} >{"<<"}</Button>
-                            <Button onClick={handelPrevPage} disabled={page <= 1} >{"<"}</Button>
-                        </ButtonGroup>
-                        <div style={{ margin: " 0 10px 0 10px" }} >
-                            <Typography variant='normal' >  {page}  </Typography>
-                        </div>
-                        <ButtonGroup variant="contained" color='secondary' size='small' aria-label="Basic button group">
-                            <Button onClick={handelNextPage} disabled={nResults / limit < page} >{">"}</Button>
-                            <Button onClick={handelLastPage} disabled={page === Math.ceil(nResults / limit)} >{">>"}</Button>
-                        </ButtonGroup>
-                    </div>
-                </CardActions>
+                <div style={{ display: "flex", alignItems: "center", width: "100%", justifyContent: "flex-end" }}>
+                    <FormControl variant="standard" >
+                <Select
+                    value={limit}
+                    onChange={(e)=>{setLimit(e.target.value)}}
+                    label="Items"
+                >
+                    <MenuItem value={5}>5</MenuItem>
+                    {nResults > 10 && (
+                        <MenuItem value={10}>10</MenuItem>
+                    )}
+                   {nResults > 30 && (
+                        <MenuItem value={20}>30</MenuItem>
+                    )}
+                    {nResults > 50 && (
+                        <MenuItem value={50}>50</MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+            <Typography sx={{ ml: 1, mr: 1 }} >{` rows of ${nResults}`}</Typography>
+            <ButtonGroup size='small' color="secondary" variant="contained" aria-label="Basic button group">
+                <Tooltip title="First page">
+                    <Button onClick={handleFirstPage} >{"<<"}</Button>
+                </Tooltip>
+                <Tooltip title="Prev page">
+                    <Button onClick={handlePrevPage} >{"<"}</Button>
+                </Tooltip>
+            </ButtonGroup>
+            <Typography sx={{ ml: 1, mr: 1 }} >{page + 1}</Typography>
+            <ButtonGroup size='small' color="secondary" variant="contained" aria-label="Basic button group">
+                <Tooltip title="Next page">
+                    <Button onClick={handleNextPage} >{">"}</Button>
+                </Tooltip>
+                <Tooltip title="Last page">
+                    <Button onClick={handleLastPage} >{">>"}</Button>
+                </Tooltip>
+            </ButtonGroup>
+                </div>
             )}
-        </Card>
+        </div>
     )
 }
