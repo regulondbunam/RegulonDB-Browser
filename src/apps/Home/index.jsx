2472dc3e1a91c74e68232cbd77c7e29f5cf3988f
galarcon-ccg <55556303+galarcon-ccg@users.noreply.github.com>
2024-05-20 23:13:13 -0600

    add home body

--- src/apps/Home/index.jsx
+++ src/apps/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Cover from './Cover'
 import InfoApp from './InfoApp'
+import { Body } from './Body'
 
 export default function Home() {
   return (
     <div>
         <Cover />
         <InfoApp />
+        <Body />
     </div>
   )
 }
