2cbbda0e9cbdef32fe865716a642ca9b830a85b0
Gabriel Alarcon <alarcongabriel128@gmail.com>
2025-07-15 14:50:34 -0600

    update Home Cover: add E. coli image to background, adjust styles for better visual appeal, and increase cover height

--- src/apps/Main/Home/Cover/index.jsx
+++ src/apps/Main/Home/Cover/index.jsx
@@ -7,14 +7,30 @@ import Search from "./Search";
 import Links from "./Links";
 import Dna from "./Dna";
 import Cards from "./Cards";
+import ecoliPNG from "./media/ecoli-img.png";
 import LogoRegulonDB from "./media/RegulonDB-logo-white.png";
 
 export default function Cover(){
     return(
         <div className={"home-cover"} >
+
             <div className={"home-cover-background"} >
                 <InteractiveCover />
-                <div style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0,0,0,0.8)", zIndex: 2 }} />
+                <div style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0,0,0,0.5)", zIndex: 2 }} />
+            </div>
+            <div className={"home-cover-background-ecoli"}
+                 style={{position: "absolute", top: 0, right: 50, zIndex: 1}}
+            >
+                <img
+                    className={"home-cover-background-ecoli-img"}
+                    src={ecoliPNG}
+                    alt="ecoli"
+                    style={{
+                        height: "420px",
+                        width: "auto",
+                        transform: "rotate(15deg)",
+                    }}
+                />
             </div>
             <div style={{zIndex: 3, position: "absolute", width: "100%"}} >
                 <img
@@ -22,7 +38,7 @@ export default function Cover(){
                     src={LogoRegulonDB}
                     alt="Logo RegulonDB"
                 />
-                <Title />
+                 <Title />
                 <div style={{display: "flex", flexDirection: "column", alignItems: "center", width: "100%"}} >
                     <MediaQuery minWidth={600}>
                         <Search />
